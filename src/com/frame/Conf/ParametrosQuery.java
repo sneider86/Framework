4378c1936add3b9f89b13d3174d3ea68ee2a063d
Erick Estrada <sneider86@gmail.com>
2016-10-07 09:49:58 -0500

    Modificaciones en los parametros

--- src/com/frame/Conf/ParametrosQuery.java
+++ src/com/frame/Conf/ParametrosQuery.java
@@ -27,6 +27,10 @@ public class ParametrosQuery {
         this.tipovalor=tipovalor;
         this.valor=valor;
     }
+
+    public ParametrosQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     /**
      * 
      * @return  Obtiene el tipo de dato del valor ingresado ejem. (int,String,Double..etc)
